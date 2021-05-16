ce206b8363774e49670516c9a70098750f71c487
mohamedoabbiit <59892187+mohamedoabbiit@users.noreply.github.com>
2021-05-16 13:42:34 -0400

    Update leap_year_function.py

--- leap_year_function.py
+++ leap_year_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+
 
 
 year = int(input("Enter a year to check if year is a leap year or not: "))
@@ -23,37 +23,10 @@ year_leap(year)
     
 
 
-# In[8]:
-
-
-2020
-
-
-# In[7]:
-
-
-2020
-
-
-# In[ ]:
 
 
 
 
-
-# In[17]:
-
-
-year = int(input("please enter a year:"))
-
-if not (year % 400 ) or (not year % 4 and year % 100):
-    print(f"{year} is a leap year")
-
-else:
-    print(f"{year} is not a leap year")
-
-
-# In[ ]:
 
 
 
